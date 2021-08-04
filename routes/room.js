f3bb1634c94bb291d334881e6349b07c3c33171b
Ruby Gracia <rubygracia1998@gmail.com>
2021-08-04 20:06:30 +0530

    enterroom

--- routes/room.js
+++ routes/room.js
@@ -33,13 +33,13 @@ router.post("/enterroom", async (req, res) => {
     userName: req.body.userName,
     roomId: req.body.roomId,
   });
-  //let roomDetail = await roomlists.find({ _id: req.body.roomId });
+  let roomDetail = await roomlists.find({ _id: req.body.roomId });
   addUser.save().then((user) => {
     res.status(200).json({
       success: true,
       data: {
         user,
-        //roomName: roomDetail[0].roomName,
+        roomName: roomDetail[0].roomName,
       },
     });
   });
